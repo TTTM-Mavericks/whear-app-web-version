bca6009a9d2ce2bf75e61c4a2237f83fbee5f06a
thuytrangvo2k2@gmail.com <thuytrangvo2k2@gmail.com>
2024-01-21 15:20:08 +0700

    [fe0014] fix about us UI

--- src/components/page/About/About.tsx
+++ src/components/page/About/About.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Story from './Story/Story';
-import Image from './Image/Image';
 import Policy from './Policy/Policy';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
