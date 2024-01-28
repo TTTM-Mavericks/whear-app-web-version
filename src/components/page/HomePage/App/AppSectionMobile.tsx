698ce24b697f917a7bca55b9f4783facaccbb7d0
thuytrangvo2k2@gmail.com <thuytrangvo2k2@gmail.com>
2024-01-28 17:23:22 +0700

    [fe0014] change mobile UI at home page

--- src/components/page/HomePage/App/AppSectionMobile.tsx
+++ src/components/page/HomePage/App/AppSectionMobile.tsx
@@ -1,31 +1,70 @@
-// AppSectionMobile.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/CloudDownload';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import './AppSectionMobile.css';
 
 const AppSectionMobile: React.FC = () => {
+  const [currentSlide, setCurrentSlide] = useState(2);
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide % 3) + 1);
+  };
+
+  const handlePrevSlide = () => {
+    setCurrentSlide((prevSlide) => ((prevSlide - 2 + 3) % 3) + 1);
+  };
+
   return (
     <section className="app-section-mobile">
-      <div className="app-image-container">
-        <img src={require('../../../../img/Introduce.jpg')} alt="App Image" className="app-image" />
-      </div>
-      <div className="center-column-mobile">
-        <h1>WHEAR APP</h1>
-        <p>Discover the latest trends and elevate your style with WHEAR. Our fashion-forward boutique offers a curated collection of designer pieces that will make you stand out from the crowd. From elegant dresses to chic accessories, we have everything you need to create your perfect look. Do not miss out on this exclusive discount - shop now and save on your favorite pieces. Download THE WHEAR APP today and let our personal stylists guide you to effortless style, everyday.</p>
-        <Button
-          variant="contained"
-          color="primary"
-          href="/download-link"
-          endIcon={<DownloadIcon />}
-          style={{ backgroundColor: 'black', color: 'white', marginTop: '20px' }}
-        >
-          Download Now
-        </Button>
-      </div>
-      <div className="app-image-container">
-        <img src={require('../../../../img/Frame.jpg')} alt="App Image" className="app-image" />
-      </div>
+      {currentSlide === 1 && (
+        <div className="app-image-container">
+          <img src={require('../../../../img/Introduce.jpg')} alt="App Image" className="app-image" />
+        </div>
+      )}
+
+      {currentSlide === 2 && (
+        <div className="center-column-mobile">
+          <h1>WHEAR APP</h1>
+          <p>Discover the latest trends and elevate your style with WHEAR. Our fashion-forward boutique offers a curated collection of designer pieces that will make you stand out from the crowd. From elegant dresses to chic accessories, we have everything you need to create your perfect look. Do not miss out on this exclusive discount - shop now and save on your favorite pieces. Download THE WHEAR APP today and let our personal stylists guide you to effortless style, everyday.Discover the latest trends and elevate your style with WHEAR. Our fashion-forward boutique offers a curated collection of designer pieces that will make you stand out from the crowd. From elegant dresses to chic accessories, we have everything you need to create your perfect look. Do not miss out on this exclusive discount - shop now and save on your favorite pieces. Download THE WHEAR APP today and let our personal stylists guide you to effortless style, everyday.</p>
+          <Button
+            variant="contained"
+            color="primary"
+            href="/download-link"
+            endIcon={<DownloadIcon />}
+            style={{ backgroundColor: 'black', color: 'white', marginTop: '20px', alignItems:'center' }}
+          >
+            Download Now
+          </Button>
+        </div>
+      )}
+
+      {currentSlide === 3 && (
+        <div className="app-image-container">
+          <img src={require('../../../../img/Frame.jpg')} alt="App Image" className="app-image" />
+        </div>
+      )}
+
+      <Button
+        variant="outlined" 
+        size="small"
+        className="arrow-button1 back-button1"
+        onClick={handlePrevSlide}
+        style={{ color:'black', borderColor: 'black', position:'absolute'}}
+      >
+        <ArrowBackIcon />
+      </Button>
+
+      <Button
+        variant="outlined" 
+        size="small"
+        className="arrow-button1 next-button1"
+        onClick={handleNextSlide}
+        style={{ color: 'black', borderColor: 'black',  position:'absolute' }}
+      >
+        <ArrowForwardIcon />
+      </Button>
     </section>
   );
 };
--- src/components/page/HomePage/Service/ServiceSectionMobile.tsx
+++ src/components/page/HomePage/Service/ServiceSectionMobile.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import './ServiceSectionMobile.css';
 
 const ServiceSectionMobile: React.FC = () => {
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide % 3) + 1);
+  };
+
+  const handlePrevSlide = () => {
+    setCurrentSlide((prevSlide) => ((prevSlide - 2 + 3) % 3) + 1);
+  };
   return (
     <section className="service-section-mobile">
       <Typography variant="h4" className="service-heading" sx={{ fontFamily: 'Poppins', fontSize: '36px', fontWeight: 'bold' }}>
         OUR SPECIALTIES
       </Typography>
       <div className="service-columns-mobile">
+      {currentSlide === 1 && (
         <div className="service-column-mobile">
             <div className="service-content-mobile">
             <Typography variant="h5" className="service-title-mobile" sx={{ fontFamily: 'Poppins', fontSize: '24px', fontWeight: 'bold' }}>
@@ -20,6 +33,9 @@ const ServiceSectionMobile: React.FC = () => {
           </div>
           <img src={require('../../../../img/service1.png')} alt="Service 1" className="service-image-mobile" />
         </div>
+        )}
+
+      {currentSlide === 2 && (
         <div className="service-column-mobile">
           <div className="service-content-mobile">
             <Typography variant="h5" className="service-title-mobile" sx={{ fontFamily: 'Poppins', fontSize: '24px', fontWeight: 'bold' }}>
@@ -31,6 +47,9 @@ const ServiceSectionMobile: React.FC = () => {
           </div>
           <img src={require('../../../../img/service2.png')} alt="Service 2" className="service-image-mobile" />
         </div>
+        )}
+
+      {currentSlide === 3 && (
         <div className="service-column-mobile">
         <div className="service-content-mobile">
             <Typography variant="h5" className="service-title-mobile" sx={{ fontFamily: 'Poppins', fontSize: '24px', fontWeight: 'bold' }}>
@@ -42,7 +61,28 @@ const ServiceSectionMobile: React.FC = () => {
           </div>
           <img src={require('../../../../img/service3.png')} alt="Service 3" className="service-image-mobile" />
         </div>
+        )}
       </div>
+
+      <Button
+        variant="outlined"
+        size="small"
+        className="arrow-button2 back-button2"
+        onClick={handlePrevSlide}
+        style={{ color: 'black', borderColor: 'black', position: 'absolute', left: 0, top:'50%' }}
+      >
+        <ArrowBackIcon />
+      </Button>
+
+      <Button
+        variant="outlined"
+        size="small"
+        className="arrow-button2 next-button2"
+        onClick={handleNextSlide}
+        style={{ color: 'black', borderColor: 'black', position: 'absolute', right: 0,top:'50%' }}
+      >
+        <ArrowForwardIcon />
+      </Button>
     </section>
   );
 };
